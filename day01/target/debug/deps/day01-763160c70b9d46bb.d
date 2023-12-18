769db6711ead33903a274d6195b56057f389220f
grumbull <michael.mcclosky@gmail.com>
2023-12-17 22:54:45 -0500

    remove target

--- day01/target/debug/deps/day01-763160c70b9d46bb.d
+++
@@ -1,8 +0,0 @@
-/Users/mike/Dev/aoc23/day01/target/debug/deps/libday01-763160c70b9d46bb.rmeta: src/lib.rs src/vec_deque_fixed.rs
-
-/Users/mike/Dev/aoc23/day01/target/debug/deps/libday01-763160c70b9d46bb.rlib: src/lib.rs src/vec_deque_fixed.rs
-
-/Users/mike/Dev/aoc23/day01/target/debug/deps/day01-763160c70b9d46bb.d: src/lib.rs src/vec_deque_fixed.rs
-
-src/lib.rs:
-src/vec_deque_fixed.rs:
--- day01/target/debug/deps/part2-93081fce1aa58129.d
+++
@@ -1,6 +0,0 @@
-/Users/mike/Dev/aoc23/day01/target/debug/deps/part2-93081fce1aa58129: src/bin/part2.rs src/bin/./input.txt
-
-/Users/mike/Dev/aoc23/day01/target/debug/deps/part2-93081fce1aa58129.d: src/bin/part2.rs src/bin/./input.txt
-
-src/bin/part2.rs:
-src/bin/./input.txt:
